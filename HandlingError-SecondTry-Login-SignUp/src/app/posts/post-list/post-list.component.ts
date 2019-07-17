c869e40562cafa52dbc9b8920d79072f43d4a32c
RajiSri486 <rajashree486@gmail.com>
2019-07-17 13:03:27 +0530

    While giving Invalid cretentials(login/signup), will give Users another try

---
+++ HandlingError-SecondTry-Login-SignUp/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PageEvent } from '@angular/material';
+
+
+import { Post } from '../post.model';
+import { PostsService } from '../post.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //  {title: 'First Posts', content: 'This is the first post\'s' },
+  //  {title: 'Second Posts', content: 'This is the second post\'s' },
+  //  {title: 'Third Posts', content: 'This is the third post\'s' }
+  // ];
+
+  posts: Post[] = [];
+  private postsSub: Subscription;
+  private authStatusSub: Subscription;
+  userIsAuthenticated = false;
+  isLoading = false;
+  totalPosts = 0;
+  postsPerPage = 2;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 10];
+  userId: string;
+
+  constructor( public postsService: PostsService, private authService: AuthService) { }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.postsService.getPosts(this.postsPerPage, 1);
+    this.userId = this.authService.getUserId();
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((postData: {posts: Post[], postCount: number}) => {
+      this.isLoading = false;
+      this.totalPosts = postData.postCount;
+      this.posts = postData.posts;
+    });
+    this.userIsAuthenticated = this.authService.getISAuth();
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
+      });
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    // console.log(pageData);
+  }
+
+  onDelete(postId: string) {
+    this.isLoading = true;
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+    this.authStatusSub.unsubscribe();
+  }
+ }
