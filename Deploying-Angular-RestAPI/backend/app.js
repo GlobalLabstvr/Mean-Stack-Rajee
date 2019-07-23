19aa37e6210dfa1e72cdbab6938f1f77f450df92
RajiSri486 <rajashree486@gmail.com>
2019-07-23 16:52:24 +0530

    Deploying Angular-RestAPI using Integrated Approach

---
+++ Deploying-Angular-RestAPI/backend/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mangoose = require('mongoose');
+const path = require('path');
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
+
+
+
+const app = express();
+
+mangoose.connect("mongodb+srv://new-user_2:" +
+  process.env.MONGO_ATLAS_PW +
+    "@cluster0-okjyw.mongodb.net/mean_db")
+.then(() => {
+  console.log("Connected to Db");
+})
+.catch(() => {
+  console.log('Connection Failed');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/', express.static(path.join(__dirname, 'angular')));
+
+app.use((req, res, next) =>{
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+    );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
+
+module.exports = app;
---
+++ Deploying-Angular-RestAPI/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://nodeangular3-env.iin4ipqkt3.us-west-2.elasticbeanstalk.com/api'
+};
